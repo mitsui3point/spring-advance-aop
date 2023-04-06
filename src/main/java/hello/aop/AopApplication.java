8816fb5036efbc1819d8ccc7f7466a5be28c9c7e
mitsui3point <rladbsgh0331@gmail.com>
2023-04-06 18:34:02 +0900

    feat($build.gradle, $AopApplication): update - library add

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -3,6 +3,10 @@ package hello.aop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 참고
+ * @Aspect 를 사용하려면 @EnableAspectJAutoProxy 를 스프링 설정에 추가해야 하지만, 스프링 부트를 사용하면 자동으로 추가된다.
+ */
 @SpringBootApplication
 public class AopApplication {
 
